42e452e42d9f0465e83ea1da1667b0b82d404b9f
Thomas Kleinöder <dev@time4oss.de>
2023-11-26 12:50:00 +0100

    default implementation for get()

--- fastapi_shared/crud/__init__.py
+++ fastapi_shared/crud/__init__.py
@@ -35,6 +35,9 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         else:
             return result
 
+    def get(self, db: Session, obj_id: any) -> ModelType | None:
+        return self._get(db, obj_id)
+
     def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data)  # type: ignore
